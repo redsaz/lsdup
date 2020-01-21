3617798bba60cdc7d6c627015f789bda42e15742
redsaz <redsaz@gmail.com>
2020-01-20 21:40:09 -0500

    Use blake3 for hashing files

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,139 @@
-use std::fs;
-use std::io;
-use std::path::Path;
+use blake3::Hash;
+use std::collections::HashMap;
 use std::env;
+use std::fs;
+use std::fs::File;
+use std::io;
+use std::io::prelude::*;
+use std::path::Path;
+use std::path::PathBuf;
+use std::vec::Vec;
+
+trait FileVisitor {
+    fn visit(&mut self, file: PathBuf);
+}
+
+struct BySizeFileVisitor {
+    size_files_map: HashMap<u64, Vec<Box<Path>>>,
+}
+
+impl BySizeFileVisitor {
+    fn new() -> BySizeFileVisitor {
+        BySizeFileVisitor {
+            size_files_map: HashMap::new(),
+        }
+    }
+}
+
+impl FileVisitor for BySizeFileVisitor {
+    fn visit(&mut self, file: PathBuf) {
+        let size = file.metadata().expect("No metadata").len();
+        println!("File: {:?} size: {} Is it a dupe? Who knows.", file, size);
+        let paths = self.size_files_map.entry(size).or_insert_with(Vec::new);
+        paths.push(file.into_boxed_path());
+    }
+}
+
+// impl IntoIterator for BySizeFileVisitor {
+//     type Item = Box<PathBuf>;
+//     type IntoIter = std::iter::Flatten<Self::Item>;
+
+//     fn into_iter(self) -> Self::IntoIter {
+//         self.size_files_map.values().filter(|v| v.len() > 1).flatten()
+//     }
+// }
+impl<'a> IntoIterator for &'a BySizeFileVisitor {
+    type Item = &'a Vec<Box<Path>>;
+    type IntoIter = std::collections::hash_map::Values<'a, u64, Vec<Box<Path>>>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.size_files_map.values()
+    }
+}
+
+struct ByHashFileVisitor {
+    hash_files_map: HashMap<Hash, Vec<Box<Path>>>,
+}
+
+impl ByHashFileVisitor {
+    fn new() -> ByHashFileVisitor {
+        ByHashFileVisitor {
+            hash_files_map: HashMap::new(),
+        }
+    }
+}
+
+impl FileVisitor for ByHashFileVisitor {
+    fn visit(&mut self, file: PathBuf) {
+        // Group all visited files by hash.
+        let hash = hash_contents(&file);
+        println!("File: {:?} hash: {} Is it a dupe? Who knows.", file, hash.to_hex());
+        let paths = self.hash_files_map.entry(hash).or_insert_with(Vec::new);
+        paths.push(file.into_boxed_path());
+    }
+}
 
 pub fn run(config: Config) -> io::Result<()> {
     // let contents = fs::read_to_string(config.filename)?;
     let dir = Path::new(&config.dir);
-    visit_dirs(dir, &print_file_info)
+    let mut dups = BySizeFileVisitor::new();
+    visit_dirs(dir, &mut dups)?;
+
+    // NOW GO THROUGH EACH ENTRY. SKIP ONES WITH ONLY ONE ITEM. HASH THE REMAINING.
+    let mut byhash = ByHashFileVisitor::new();
+    for x in &dups {
+        if x.len() < 1 {
+            continue;
+        }
+
+        for y in x {
+            byhash.visit(y.to_path_buf());
+        }
+    }
+
+    // THEN GO THROUGH *THOSE* ENTRIES. SKIP ONES WITH ONLY ONE ITEM. WHAT REMAINS ARE DUPES.
+    // ORDER THEM AS APPROPRIATE AND OUTPUT THE RESULTS.
 
     // for line in results {
     //     println!("{}", line);
     // }
 
-    // Ok(())
+    Ok(())
+}
+
+fn hash_contents(file: &PathBuf) -> Hash {
+    let mut out = File::open(file).unwrap();
+    let mut buf = [0; 128 * 1024];
+    let mut hasher = blake3::Hasher::new();
+    loop {
+        let length = out.read(&mut buf).unwrap();
+        if length == 0 {
+            break;
+        }
+        hasher.update(&buf);
+    }
+
+    hasher.finalize()
 }
 
 fn print_file_info(file: &fs::DirEntry) {
-    println!("File: {:?} size: {}", file.path(), file.metadata().expect("No metadata").len());
+    println!(
+        "File: {:?} size: {}",
+        file.path(),
+        file.metadata().expect("No metadata").len()
+    );
 }
 
-fn visit_dirs(dir: &Path, callback: &dyn Fn(&fs::DirEntry)) -> io::Result<()> {
+fn visit_dirs(dir: &Path, visitor: &mut dyn FileVisitor) -> io::Result<()> {
     if dir.is_dir() {
         for entry in fs::read_dir(dir)? {
             let entry = entry?;
             let path = entry.path();
             let metadata = entry.metadata()?;
             if path.is_dir() && metadata.is_dir() {
-                visit_dirs(&path, callback)?;
+                visit_dirs(&path, visitor)?;
             } else {
-                callback(&entry);
+                visitor.visit(path);
             }
         }
     }
@@ -55,14 +160,12 @@ impl Config {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::io::prelude::*;
-    use std::fs::File;
 
     #[test]
     fn visit_dirs_test_dir() {
         let dir = Path::new("./target/test_dir");
         std::fs::remove_dir_all(dir).unwrap_or_else(|error| {
-            if error.kind() != io::ErrorKind::NotFound{
+            if error.kind() != io::ErrorKind::NotFound {
                 panic!("Problem removing old directory: {:?}", error);
             }
         });
@@ -89,12 +192,15 @@ mod tests {
             if length == 0 {
                 break;
             }
-            hasher.update(& buf);
+            hasher.update(&buf);
         }
         let hash1 = hasher.finalize();
 
         // let hash1 = blake3::hash(b"foobarbaz");
         println!("hex: {}", hash1.to_hex());
-        assert_eq!("fcc85134f1e140988a686dbd857f9dcf453cfbfc986f0fcfbb987a0436a1cd42", hash1.to_hex().as_str());
+        assert_eq!(
+            "fcc85134f1e140988a686dbd857f9dcf453cfbfc986f0fcfbb987a0436a1cd42",
+            hash1.to_hex().as_str()
+        );
     }
 }
